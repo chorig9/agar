e9588ce8edd02f31562fbf95f8380260cf734aa5
chorig9 <chorig9@gmail.com>
2017-02-24 15:07:50 +0100

    init

---
+++ desktop/src/com/game/agar/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.game.agar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.game.agar.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
