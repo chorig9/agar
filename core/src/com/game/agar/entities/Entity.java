1809cc3bbcf3e2473893ca2d82893c0ddd83160c
zegmil <zegmilosz@gmail.com>
2017-03-01 15:56:51 +0100

    Circle collision and growth on ShapeRenderer.

--- core/src/com/game/agar/entities/Entity.java
+++ core/src/com/game/agar/entities/Entity.java
@@ -1,28 +1,89 @@
 package com.game.agar.entities;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.game.agar.tools.Position;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public abstract class Entity {
 
-    Position position;
-    Sprite sprite;
+    private static Random random = new Random();
+    protected Position position;
+    protected float radius;
+    protected int weight;
+    protected Color color;
 
-    Entity(Position position, Sprite sprite){
+    Entity(Position position, int weight){
         this.position = position;
-        this.sprite = sprite;
+        this.weight = weight;
+        this.radius = (float)Math.sqrt(weight/Math.PI);
+        float red = random.nextFloat();
+        float green = random.nextFloat();
+        float blue = random.nextFloat();
+        Color randomColor = new Color(red,green,blue,1);
+        this.color = randomColor;
+
     }
 
     public Position getPosition() {
         return position;
     }
+    public float getRadius() { return radius; }
+    public int getWeight() { return weight; }
+    public Color getColor() { return color; }
 
-    public void setPosition(Position position) {
-        this.position = position;
+    public void updateRadius(){
+        radius = (float)Math.sqrt(weight/Math.PI);
     }
 
-    public Sprite getSprite() {
-        sprite.setPosition(position.x - sprite.getWidth() / 2, position.y - sprite.getHeight() / 2);
-        return sprite;
+    public boolean isCollision(Entity collidingObject){
+        double dx = Math.abs(this.position.x - collidingObject.getPosition().x);
+        double dy = Math.abs(this.position.y - collidingObject.getPosition().y);
+        double distance = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
+        if(distance < this.radius + collidingObject.getRadius())
+            return true;
+        else
+            return false;
+    }
+
+    public Entity getEatenEntity(Entity collidingObject){
+        if(this.weight <= collidingObject.getWeight()) {
+            collidingObject.eat(this);
+            return this;
+        }
+        else
+        {
+            this.eat(collidingObject);
+            return collidingObject;
+        }
+    }
+
+    public void eat (Entity collidingObject){
+        int massGained = collidingObject.getWeight();
+        Timer timer = new Timer();
+        TimerTask growth = new TimerTask() {
+            int startWeight = weight;
+            int gain = 1 + massGained/1000;
+            @Override
+            public void run() {
+                if (weight == startWeight + massGained) {
+                    cancel();
+                    timer.cancel();
+                    timer.purge();
+                }
+                else
+                {
+                    if(weight + gain <=  startWeight + massGained)
+                        weight += gain;
+                    else
+                        weight = startWeight + massGained;
+                    updateRadius();
+                }
+            }
+        };
+        timer.schedule(growth,0,1);
     }
 }
--- core/src/com/game/agar/entities/Player.java
+++ core/src/com/game/agar/entities/Player.java
@@ -6,28 +6,26 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.game.agar.tools.Position;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Player extends Entity {
 
     private double movingDirectionAngle;
 
-    public Player(Position position) {
-
-        super(position, null);
-
-        Pixmap pixmap = new Pixmap(32,32, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.RED);
-        pixmap.fillCircle(16,16,12);
-        Texture texture = new Texture(pixmap);
-
-        sprite = new Sprite(texture);
+    public Player(Position position, int weight) {
+        super(position, weight);
+        color = Color.RED;
     }
 
-    public void setMovingDirection(double angle){
+    public void setMovingDirection(double angle) {
         this.movingDirectionAngle = angle;
     }
 
-    public void move(double translation){
+    public void move(double translation) {
         position.x += translation * Math.cos(movingDirectionAngle);
         position.y += translation * Math.sin(movingDirectionAngle);
     }
+
 }
