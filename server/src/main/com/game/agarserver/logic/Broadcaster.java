48679ee3a1ccbdf43109e39163d885c49a927f3c
wkalinski <wkalinski6@wp.pl>
2017-03-08 20:14:59 +0100

    Integrated server to the project

---
+++ server/src/main/com/game/agarserver/logic/Broadcaster.java
@@ -0,0 +1,25 @@
+package com.game.agarserver.logic;
+
+import com.game.agarserver.main.logic.User;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Broadcaster implements Consumer<JSONObject> {
+
+    private List<User> users;
+
+    public Broadcaster(List<User> users){
+        this.users = users;
+    }
+
+    @Override
+    public void accept(JSONObject packet) {
+        for(User user : users){
+            // TODO - check if user is in close enough to see this event
+
+            user.sendPacket(packet);
+        }
+    }
+}
